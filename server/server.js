ebfb59b6e1846b5f2c8b4adbc6730ab38bd58dfd
GiaHuy1500 <giahuy.lieu@griffithuni.edu.au>
2024-09-02 20:33:46 +1000

    Create server side

---
+++ server/server.js
@@ -0,0 +1,15 @@
+var cors = require('cors');
+var express = require('express'); // import express.js
+var app = express();
+var bodyParser = require('body-parser');
+var path = require('path')
+// Cross origin resource sharing to cater for port 4200 to pot 3000
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(express.static(__dirname + "../dist/3813-ict-assignment"))
+
+var http = require('http').Server(app)
+
+
+
