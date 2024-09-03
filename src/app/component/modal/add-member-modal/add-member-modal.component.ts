530b86a294a5bf7c4cadc0d000127ada9faf9618
GiaHuy1500 <giahuy.lieu@griffithuni.edu.au>
2024-09-04 09:38:24 +1000

    added create and delete user (groups) template

--- src/app/component/modal/add-member-modal/add-member-modal.component.ts
+++ src/app/component/modal/add-member-modal/add-member-modal.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormGroup, FormsModule } from '@angular/forms';
+import { UserService } from '../../../service/user.service';
 @Component({
   selector: 'app-add-member-modal',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './add-member-modal.component.html',
   styleUrl: './add-member-modal.component.css'
 })
 export class AddMemberModalComponent {
+  
+  @ViewChild('modal', { static: false }) modal: any;
+  username:any=""
+  role:any=""
+  email:any=""
+  password:any=""
+  constructor(private userService:UserService){}
+  ngAfterViewInit(){
+  
+  }
+  openModal() {
+    const modalElement = this.modal.nativeElement;
+    modalElement.style.display = 'flex';
+  }
 
+  closeModal() {
+    const modalElement = this.modal.nativeElement;
+    modalElement.style.display = 'none';
+  }
+  onSubmit(){
+    let userObj = {"username": this.username, "role": this.role, "email":this.email}
+    this.userService.createUser(userObj)
+    .subscribe((data:any)=>{
+      console.log(data)
+    })
+    console.log(userObj)
+    this.closeModal()
+  }
 }
